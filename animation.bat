f0480b9ce1bcbe03b1622d836c9499ff572cb68f
Stupidteaman <167263178+Stupidteaman@users.noreply.github.com>
2024-09-14 16:21:09 -0400

    Add files via upload

---
+++ animation.bat
@@ -0,0 +1,86 @@
+@echo off
+echo H
+timeout 1
+cls
+echo Hi
+timeout 1
+cls
+echo Hi!
+timeout 2
+cls
+echo I
+timeout 0.5
+cls
+echo I am
+timeout 0.5
+cls
+echo I am ho
+timeout 0.5
+cls
+echo I am hol
+timeout 0.5
+cls
+echo I am hold
+timeout 0.5
+cls
+echo I am holdi
+timeout 0.5
+cls
+echo I am holdin
+timeout 0.5
+cls
+echo I am holding
+timeout 0.5
+cls
+echo I am holding y
+timeout 0.5
+cls
+echo I am holding yo
+timeout 0.5
+cls
+echo I am holding you
+timeout 0.5
+cls
+echo I am holding your
+timeout 0.5
+cls
+echo I am holding your m
+timeout 0.5
+cls
+echo I am holding your mo
+timeout 0.5
+cls
+echo I am holding your mom
+timeout 0.5
+cls
+echo I am holding your mom h
+timeout 0.5
+cls
+echo I am holding your mom ho
+timeout 0.5
+cls
+echo I am holding your mom hos
+timeout 0.5
+cls
+echo I am holding your mom host
+timeout 0.5
+cls
+echo I am holding your mom hosta
+timeout 0.5
+cls
+echo I am holding your mom hostag
+timeout 0.5
+cls
+echo I am holding your mom hostage
+timeout 0.5
+cls
+echo I am holding your mom hostage r
+timeout 0.5
+cls
+echo I am holding your mom hostage rn
+timeout 0.5
+cls
+echo I am holding your mom hostage rn uwu
+timeout 1
+cls
+pause
